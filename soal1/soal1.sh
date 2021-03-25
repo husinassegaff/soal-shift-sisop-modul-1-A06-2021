4e16cf7f5a8e7dd473e7a41574c1d159097e4e08
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2021-03-25 23:40:15 +0700

    updated no 1a

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr > text1.txt
-paste <(cut -c 9-50 text1.txt) <(cut -c 1-8 text1.txt)
+paste <(cut -c 9-50 text1.txt) <(cut -c 1-8 text1.txt) > error_message.csv
