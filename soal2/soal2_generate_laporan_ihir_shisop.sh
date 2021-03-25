af3bcd6333a49140f71cce3c2677bfdbcd7fae4f
osd0081 <m.rafki2927@gmail.com>
2021-03-25 21:55:04 +0700

    Hapus file hasil.txt

---
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -0,0 +1,13 @@
+#Transaksi terakhir dengan profit percentage terbesar yaitu *ID Transaksi* 
+#dengan persentase *Profit Percentage*%.
+
+#Daftar nama customer di Albuquerque pada tahun 2017 antara lain:
+#*Nama Customer1*
+#*Nama Customer2* dst
+
+#Tipe segmen customer yang penjualannya paling sedikit adalah *Tipe Segment* 
+#dengan *Total Transaksi* transaksi.
+
+#Wilayah bagian (region) yang memiliki total keuntungan (profit) yang paling 
+#sedikit adalah *Nama Region* dengan 
+#total keuntungan *Total Keuntungan (Profit)*
