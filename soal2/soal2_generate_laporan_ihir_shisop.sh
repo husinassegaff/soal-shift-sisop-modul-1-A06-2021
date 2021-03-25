e4f00fae9bbd86b1be7692af046a2dbb8dd7a75a
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2021-03-25 23:14:10 +0700

    Menambahkan 2a

---
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+awk 'BEGIN {FS = "\t";} 
+{
+    if(NR != 1){
+        profitPercentage = ($21 / ($18 - $21)) * 100;
+
+        if(maxProfitPercentage <= profitPercentage){
+            maxProfitPercentage = profitPercentage;
+            orderId = $2;
+            row = $1;
+        }
+    } 
+}
+
+
+END {
+    printf ("Transaksi terakhir dengan profit percentage terbesar yaitu %s dengan persentase %.2f%%\n\n", orderId, maxProfitPercentage);
+    
+    
+    }' Laporan-TokoShiSop.tsv
