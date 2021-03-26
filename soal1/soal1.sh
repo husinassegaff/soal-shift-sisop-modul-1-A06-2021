c955ea115f1f0037f6cc48b2397f30ca8acebe2f
Richard Asmarakandi <soraasmarakandi@gmail.com>
2021-03-26 22:23:25 +0700

    Updated soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#no1a
+
+grep -oP "(INFO.*)|(ERROR.*)" syslog.log 
+
+#no1b
+
+grep -oP '(?<=ERROR ).*(?= )' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 1-2 > temps1b1.txt
+grep -oP '(?<=ERROR ).*(?= )' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 4-99 > temps1b2.txt
+paste -d '\t\t' temps1b2.txt temps1b1.txt
+
+#no1c
+
+
+#no1d
+
 grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr | cut -b 6-7 > temp1.txt
 grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr | cut -b 9-50 > temp2.txt
 sed 's/$/ ,/' temp2.txt > temp3.txt
