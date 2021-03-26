a9744b2983de9112f7eae255bbf46bbf6cde4429
Richard Asmarakandi <soraasmarakandi@gmail.com>
2021-03-26 07:54:25 +0700

    Updated soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr > text1.txt
-paste <(cut -c 9-50 text1.txt) <(cut -c 1-8 text1.txt) > error_message.csv
+grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr | cut -b 6-7 > temp1.txt
+grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr | cut -b 9-50 > temp2.txt
+sed 's/$/ ,/' temp2.txt > temp3.txt
+printf "Error,Count\n" > error_message.csv
+paste temp3.txt temp1.txt >> error_message.csv
+
