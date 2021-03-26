f0ac621db38951ba318d1a26192ac37574395e31
Richard Asmarakandi <soraasmarakandi@gmail.com>
2021-03-26 23:33:01 +0700

    Updated soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -6,12 +6,22 @@ grep -oP "(INFO.*)|(ERROR.*)" syslog.log
 
 #no1b
 
-grep -oP '(?<=ERROR ).*(?= )' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 1-2 > temps1b1.txt
-grep -oP '(?<=ERROR ).*(?= )' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 4-99 > temps1b2.txt
-paste -d '\t\t' temps1b2.txt temps1b1.txt
+grep -oP '(ERROR).*(?= )' syslog.log;
+printf "Total Error = ";
+grep -c 'ERROR' syslog.log;
 
 #no1c
 
+grep -oP '(?=\().*' syslog.log > s1c1.txt
+grep -oP '(ERROR|INFO)' syslog.log > s1c2.txt
+paste s1c1.txt s1c2.txt > s1c3.txt
+tr -d '\r' < s1c3.txt > s1c4.txt
+column -s $'\t' -t < s1c4.txt > s1c5.txt
+sort < s1c5.txt | uniq -c | cut -b 7-80> s1jadi.txt
+cut -b 1 < s1jadi.txt > s1count.txt
+cut -b 18-23 < s1jadi.txt > s1errorinfo.txt
+cut -b 3-16 < s1jadi.txt > s1username.txt
+paste s1username.txt s1errorinfo.txt s1count.txt
 
 #no1d
 
