8a42dbe32b7c3e9e0a1ad5bc0fe903641c720d27
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2021-03-31 22:45:11 +0700

    updated 1b, 1c, and 1e

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -6,18 +6,18 @@ grep -oP "(INFO.*)|(ERROR.*)" syslog.log
 
 #nomor1b
 
-grep -oP '(?<=ERROR).*(?=\\()' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 1-2 > temps1b1.txt
-grep -oP '(?<=ERROR).*(?=\\()' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 4-99 > temps1b2.txt
+grep -oP '(?<=ERROR).*(?=\ \()' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 1-2 > temps1b1.txt
+grep -oP '(?<=ERROR).*(?=\ \()' syslog.log | sort |  uniq -c | cut -b 6-99 | sort -nr | cut -b 4-99 > temps1b2.txt
 paste -d '\t\t' temps1b2.txt temps1b1.txt
 
 #nomor1c
 
 printf "Username,INFO,ERROR\n"
-user=$(grep -rohP "(\([a-zA-Z.]+\))" syslog.log | sort | uniq | grep -oP "(?<=\().*(?=\))")
+user=$(grep -oP "(?<=\().*(?=\))" syslog.log | sort | uniq)
 
 for i in $user
 do
-     printf "%s,%d,%d\n" $i $(grep -cP "INFO.*$i" syslog.log) $(grep -cP "ERROR.*$i" syslog.log);
+     printf "%s,%d,%d\n" $i $(grep -cP "INFO.*(?<=\()($i)" syslog.log) $(grep -cP "ERROR.*(?<=\()($i)" syslog.log);
 done
 
 #nomor1d
@@ -31,10 +31,10 @@ paste temp3.txt temp1.txt >> error_message.csv
 #nomor1e
 
 printf "Username,INFO,ERROR\n" > user_statistic.csv
-user=$(grep -rohP "(\([a-zA-Z.]+\))" syslog.log | sort | uniq | grep -oP "(?<=\().*(?=\))")
+user=$(grep -oP "(?<=\().*(?=\))" syslog.log | sort | uniq)
 
 for i in $user
 do
-    printf "%s,%d,%d\n" $i $(grep -cP "INFO.*$i" syslog.log) $(grep -cP "ERROR.*$i" syslog.log);
+    printf "%s,%d,%d\n" $i $(grep -cP "INFO.*(?<=\()($i)" syslog.log) $(grep -cP "ERROR.*(?<=\()($i)" syslog.log);
 done | sort >> user_statistic.csv;
 
